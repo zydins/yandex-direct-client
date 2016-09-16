fd7a980dcbdc2f8e50a76ee40263a17eea90a6f4
zydins <zydins@gmail.com>
2016-09-16 11:38:53 +0300

    fix empty param

--- src/main/java/ru/cultserv/adv/yandex/direct/v5/methods/Ads.java
+++ src/main/java/ru/cultserv/adv/yandex/direct/v5/methods/Ads.java
@@ -12,25 +12,25 @@ public interface Ads {
     List<OperationResult> add(List<AdInfo> ads);
 
     @WithConverter(entity = {"SelectionCriteria", "Ids"}, flatten = true)
-    List<OperationResult> archive();
+    List<OperationResult> archive(List<Long> ids);
 
     @WithConverter(entity = {"SelectionCriteria", "Ids"}, flatten = true)
-    List<OperationResult> delete();
+    List<OperationResult> delete(List<Long> ids);
 
     @WithConverter(converter = ParamConverter.SINGLE_PARAM_CONVERTER, flatten = true)
     List<AdInfo> get(AdRequest request);
 
     @WithConverter(entity = {"SelectionCriteria", "Ids"}, flatten = true)
-    List<OperationResult> moderate();
+    List<OperationResult> moderate(List<Long> ids);
 
     @WithConverter(entity = {"SelectionCriteria", "Ids"}, flatten = true)
-    List<OperationResult> resume();
+    List<OperationResult> resume(List<Long> ids);
 
     @WithConverter(entity = {"SelectionCriteria", "Ids"}, flatten = true)
-    List<OperationResult> suspend();
+    List<OperationResult> suspend(List<Long> ids);
 
     @WithConverter(entity = {"SelectionCriteria", "Ids"}, flatten = true)
-    List<OperationResult> unarchive();
+    List<OperationResult> unarchive(List<Long> ids);
 
     @WithConverter(entity = "Ads", flatten = true)
     List<OperationResult> update(List<AdInfo> ads);
